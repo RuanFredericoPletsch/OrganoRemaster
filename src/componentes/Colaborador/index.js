79d70e597dbcb3c911d384299c558e147f754321
unknown <ruanpletsch@gmail.com>
2023-09-05 08:42:40 -0300

    Criando função de Delete para colaboradores

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,9 @@
+import { AiFillCloseCircle } from 'react-icons/ai'
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
+        <AiFillCloseCircle size={25} className='deletar' onClick={aoDeletar} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
@@ -12,4 +14,5 @@ const Colaborador = ({ colaborador, corDeFundo }) => {
     </div>)
 }
 
+export {}
 export default Colaborador
